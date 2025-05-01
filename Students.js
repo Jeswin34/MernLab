ffa45df905ee7f3cc6948785b4e5c31f5d23a6d8
Jeswin34 <jeswinj22@karunya.edu.in>
2025-05-01 22:51:54 +0530

    Create Students.js

---
+++ Students.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    regno: String,
+    cgpa: Number,
+    dept: String
+})
+const studentModel = mongoose.model("Students", studentSchema, "Students")
+
+module.exports = studentModel
